d2e429e5c6b56d975a94a8a764545ab24122b42d
noah <noahwoodward2244@gmail.com>
2018-08-04 14:27:07 -0700

    Added New Stuff XCWorkspace

--- Knapsack/Model/ClothingModelLogic.swift
+++ Knapsack/Model/ClothingModelLogic.swift
@@ -35,16 +35,16 @@ struct ClothingModelLogic{
         case .female:
             switch temp {
             case 100...1000:
-                return["Light Colored Tee Shirt","Athletic Shorts","Sandals","Safari Hat","Sneakers", "Bathing Suit", "Short Sleeved Night Shirt","Loose Underwear", "Short Cut Socks", "Sunglasses", "Cargo Shorts", "Polo Shirt", "Short Sleeve Button Up"]
+                return["Tank Tops","T-Shirts","Underwear","Socks","Bra","Gym Bra","Bathing Suit","Casual Shorts", "Short Sleeve Dress Shirt","Dress Shorts","Sun Hat","Sunglasses","Summer Dress","Sneakers","Flip Flops"]
             case 90...99:
-                return["Casual Dress","Dress shorts","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans"]
+                return["Casual Dress","Dress shorts","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans","Summer Dress","Dress Shoes","Formal Dress","Sun Hat","Casual Socks","Liners","Flip Flops"]
             case 75...89:
-                return["Casual Dress","Dress shorts","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans"]
+                return["Casual Dress","Summer Dress","Windbreaker","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans","Yoga Pants","Casual Socks","Liners","Flip Flops","Sweatshirt","Sunglasses","Hat for the Sun"]
             case 60...74:
-                return["Casual Dress","Dress shorts","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans"]
+                return["Casual Dress","Summer Dress","Windbreakers","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans","Yoga Pants","Casual Socks","Liners","Flip Flops","Sweatshirt","Sunglasses","Hat to block wind"]
 
             case 50...59:
-                 return["Casual Dress","Dress shorts","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans"]
+                 return["Casual Dress","Dress shorts","Dress Pants","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans"]
             case -50...49:
                  return["Casual Dress","Dress shorts","Underwear","Bra","Sandals","Cardigan","Leggings","Short Sleeved Shirt","Bathing Suit","Light Sweater","Sneakers","Athletic Shirt","Sports Bra","Long Sleeve Shirt","Jeggings","Jeans"]
             
@@ -59,3 +59,5 @@ struct ClothingModelLogic{
     
     }
 }
+// Do Error Handling
+// Show Location
